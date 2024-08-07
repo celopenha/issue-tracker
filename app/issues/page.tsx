17f594a53205c964493c2f6077876871cb72ff59
Marcelo Penha <niqcelo@gmail.com>
2024-08-07 15:53:38 -0300

    get all issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,49 @@
-import { Flex, Text, Button } from "@radix-ui/themes";
-import { TextField } from "@radix-ui/themes";
+import prisma from "@/prisma/client";
+import { Flex, Text, Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
+import IssueStatusBadge from "../_components/IssueStatusBadge";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
   return (
     <>
       <h1>List of issues</h1>
-      <Link href="/issues/new">
-        <Button>Click here to create an issue</Button>
-      </Link>
+      <div className="mb-5">
+        <Button>
+          <Link href="/issues/new">Click here to create an issue</Link>
+        </Button>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}{" "}
+                <div className="block md:hidden text-neutral-500">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </>
   );
 };
