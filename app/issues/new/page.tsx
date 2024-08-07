ea0aa903e045fe704aac49040b203eb77b4ce01f
Marcelo Penha <niqcelo@gmail.com>
2024-08-07 02:20:49 -0300

    handle new create issue errors and add more validation on api

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { TextField, TextArea, Button } from "@radix-ui/themes";
+import { TextField, TextArea, Button, Callout } from "@radix-ui/themes";
 import React, { useState } from "react";
 import { CiTextAlignLeft } from "react-icons/ci";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { MdErrorOutline } from "react-icons/md";
 import { useRouter } from "next/navigation";
-import { toast } from "react-hot-toast";
 
 interface IssueForm {
   title: string;
@@ -17,30 +17,43 @@ interface IssueForm {
 const NewIssuePage = () => {
   const { register, control, handleSubmit } = useForm<IssueForm>();
   const router = useRouter();
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        const response = await axios.post("/api/issues", data);
-        if (response.status === 201) {
-          router.push("/issues");
-        }
-      })}
-    >
-      <TextField.Root placeholder="Title" {...register("title")}>
-        <TextField.Slot>
-          <CiTextAlignLeft />
-        </TextField.Slot>
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => <SimpleMDE {...field} />}
-      />
-
-      <Button>Submit new Issue</Button>
-    </form>
+    <>
+      <form
+        className="max-w-xl space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            const response = await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            console.error(error);
+            setError("An unexcpected error occurred");
+          }
+        })}
+      >
+        <TextField.Root placeholder="Title" {...register("title")}>
+          <TextField.Slot>
+            <CiTextAlignLeft />
+          </TextField.Slot>
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => <SimpleMDE {...field} />}
+        />
+        {error && (
+          <Callout.Root color="red">
+            <Callout.Icon>
+              <MdErrorOutline />
+            </Callout.Icon>
+            <Callout.Text>{error}</Callout.Text>
+          </Callout.Root>
+        )}
+        <Button>Submit new Issue</Button>
+      </form>
+    </>
   );
 };
 
