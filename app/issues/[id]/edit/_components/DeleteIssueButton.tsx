1d2f02f6dc740e33420023dbad4cd6dfb8c73022
Marcelo Penha <niqcelo@gmail.com>
2024-08-07 20:20:39 -0300

    show confirmation modal

--- app/issues/[id]/edit/_components/DeleteIssueButton.tsx
+++ app/issues/[id]/edit/_components/DeleteIssueButton.tsx
@@ -1,18 +1,39 @@
-import prisma from "@/prisma/client";
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
+"use client"
+
+import { Button, AlertDialog, Flex } from "@radix-ui/themes";
 import { MdDeleteOutline } from "react-icons/md";
 
-const DeleteIssueButton =  ({ issueId }: { issueId: number }) => {
-  // async function deleteIssue() {
-  //   await prisma.issue.delete({ where: { id: issueId } });
-  // }
-
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red" >
-      <MdDeleteOutline />
-      Delete issue
-    </Button>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">
+            <MdDeleteOutline />
+            Delete issue
+          </Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content maxWidth="450px">
+          <AlertDialog.Title>Revoke access</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            Are you sure? This action can not be undone.
+          </AlertDialog.Description>
+
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red">
+                Confirm Deletion
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
